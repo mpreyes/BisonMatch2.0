cb50691b38eab7f9d7630c45032bc088228397bc
[madepreyes] <[reyes.madelyn.mr@gmail.com]>
2018-01-02 20:12:01 -0600

    initial shell

---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="A fundrasing effort Lipscomb's ACM chapter">
+    <meta name="author" content="Lipscomb University student ACM chapter">
+    <link rel="icon" href="favicon.ico">
+
+    <title>BisonMatch</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/global.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+    <style>
+
+
+
+    </style>
+  </head>
+
+  <body>
+
+    <header>
+      <!-- Fixed navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php">BisonMatch</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="results.php">Results</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About BisonMatch</a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    </header>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+      <div class="mt-3">
+        <h1 class="text-center">Welcome to BisonMatch! Version 2.0</h1>
+      </div>
+
+
+
+      <div class="container">
+ &nbsp;Bison Match is a Valentine's Day matching service only available to Lipscomb students. It is hosted by Lipscomb's very own College of Computing and Technology as a project for members of our local chapter of ACM - the Association for Computing Machinery.
+ It is designed to match you up with the perfect person to spend Valentine's Day with, and maybe even the rest of your life (this is Lipscomb after all)!
+      </div>
+
+      <div class="container text-center">
+        <p class="lead"> Ready to start? </p>
+        <a href="bmQuiz.php"><button type="button" class="btn btn-danger">Find me love</button></a>
+
+      </div>
+
+    </main>
+
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">&copy;2018 Lipscomb University's student ACM chapter</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
